e2c017b612b8c3bfc2976ab7b509043788e572b6
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-07-16 09:01:01 +0300

    write a simple program

--- Program.cs
+++ Program.cs
@@ -1,14 +1,5 @@
 ﻿using System;
 
-namespace csharp_learner
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("What is your name?");
-            string str = Console.ReadLine();
-            Console.WriteLine("Hello " + str);
-        }
-    }
-}
+Console.WriteLine("What is your name?");
+string str = Console.ReadLine();
+Console.WriteLine("Hello " + str);
